4af85d0800d30d4dc75becd49d3d9c1a8a6fbc8d
mpr0xy <mpr0xy@live.com>
2014-12-01 23:42:55 +0800

    重写register.html页面

--- front/app/scripts/main.js
+++ front/app/scripts/main.js
@@ -1,7 +1,25 @@
 // register
 $(function(){
   $('.register').css({
-    'min-height': window.innerHeight + 'px',
-    'padding-top': (window.innerHeight / 10) + 'px'
+    'min-height': window.innerHeight + 'px'
   });
+
+  var width = window.innerWidth * 0.7;
+  if (width > 600){
+    width = 600;
+  }
+  var drawPassword = new NinePointPassword({
+    CW: width,
+    CH: width * 1,
+    OffsetX: width * 0.08,
+    OffsetY: width * 0.08,
+    R: width * 0.1,
+    color: "#fe90aa",
+    dom: $("#draw_password").get(0),
+    callback: function(password){
+      console.log(password);
+    }
+  });
+
+  drawPassword.start();
 });

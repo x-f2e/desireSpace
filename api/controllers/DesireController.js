ae64563cd8b4b6a6c44a4772a92f0b93a18b93c1
mpr0xy <mpr0xy@live.com>
2014-12-03 23:51:38 +0800

    remove old code

--- api/controllers/DesireController.js
+++ api/controllers/DesireController.js
@@ -6,19 +6,6 @@
  */
 
 module.exports = {
-	index: function(request, response) {
-    if (typeof request.session.userCount == 'undefined'){
-      request.session.userCount = 0;
-    }
-    else{
-      request.session.userCount += 1;
-    }
-    response.cookie("userCount", request.session.userCount);
-    response.send("xxoo");
-  },
 
-  show: function(request, response) {
-    response.send("show xxoo");
-  }
 };
 
